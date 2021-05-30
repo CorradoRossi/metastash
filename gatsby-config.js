b78cf356d7a00b583b88c6cf7f862915f5fcacce
Rossi Meacham <rossi@frugaltricks.com>
2021-05-30 14:22:24 -0400

    gitignore and icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Node.js (TypeScript) API',
+    title: "Gatsby + Node.js (TypeScript) API",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Gatsby + Node.js (TypeScript) API',
-        short_name: 'Gatsby + Node.js (TypeScript)',
-        start_url: '/',
-        icon: 'src/images/gatsby-icon.png',
+        name: "Gatsby + Node.js (TypeScript) API",
+        short_name: "Gatsby + Node.js (TypeScript)",
+        start_url: "/",
+        icon: "src/images/metastash.png",
       },
     },
   ],
