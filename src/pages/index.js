3f254172927d9e914913607a6d8ebbfcb1ed0dde
Rossi Meacham <rossi@frugaltricks.com>
2021-06-01 15:02:08 -0400

    change index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Helmet } from 'react-helmet';
-import '../styles/index.css';
+import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
+import "../styles/index.css";
 
 function Index() {
   const [date, setDate] = useState(null);
   useEffect(() => {
     async function getDate() {
-      const res = await fetch('/api/date');
+      const res = await fetch("/api/date");
       const newDate = await res.text();
       setDate(newDate);
     }
@@ -15,40 +15,25 @@ function Index() {
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>Metastash</title>
       </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
+      <h1>Metastash</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
+        Deployed by{" "}
+        <a href="https://rssi.dev" target="_blank" rel="noreferrer noopener">
+          @Metastash
         </a>
-        !
       </h2>
       <p>
         <a
-          href="https://github.com/vercel/vercel/tree/main/examples/gatsby"
+          href="https://github.com/CorradoRossi/"
           target="_blank"
           rel="noreferrer noopener"
         >
           This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
+        </a>{" "}
+        is a <a href="https://www.rssi.dev/">Metastash</a> app
       </p>
-      <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
 }
