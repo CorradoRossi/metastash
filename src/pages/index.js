3617e9a3ba4befdf6cf47e222f1206dd7dd0e3e8
Rossi Meacham <rossi@frugaltricks.com>
2021-06-03 01:26:00 -0400

    spruce it up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import "../styles/index.css";
+import logo from "../images/metastash.png";
 
 function Index() {
   const [date, setDate] = useState(null);
@@ -17,7 +18,11 @@ function Index() {
       <Helmet>
         <title>Metastash</title>
       </Helmet>
-      <h1>Metastash</h1>
+      <div className="logo-wrapper">
+        <h1>Metastash</h1>
+        <img src={logo} height="60" />
+      </div>
+      <a href="https://discord.gg/U2sKKjqZ2n">Join the Discord</a>
     </main>
   );
 }
