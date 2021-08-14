20a6e9d3eae460d41a0176eeaebd714f5d8a8bf5
Rossi Meacham <rossi@frugaltricks.com>
2021-08-13 20:23:27 -0400

    major overhaul

--- lib/web3/fetch-unique-order.ts
+++ lib/web3/fetch-unique-order.ts
@@ -1,16 +1,23 @@
 import { concat, isEmpty } from 'lodash';
 import { apiGetAccountUniqueOrders as getOrders } from './opensea-api-order';
-import { UNIQUE_TOKENS_LIMIT_PER_PAGE as uniqueLimit, UNIQUE_TOKENS_LIMIT_TOTAL as totalLimit } from '@lib/constants';
+import {
+  UNIQUE_TOKENS_LIMIT_PER_PAGE as uniqueLimit,
+  UNIQUE_TOKENS_LIMIT_TOTAL as totalLimit
+} from '@lib/constants';
 
-export const fetchOrders = async (user: any, assets: object[], setAssets: Function, address: string) => {
-  let localAddress: string = address;
+export const fetchOrders = async (
+  user: any,
+  assets: object[],
+  setAssets: Function,
+  address: string
+) => {
   let batch: boolean = isEmpty(assets);
   let stop: boolean = false;
   let unique: any[] = [];
   let handle: any = null;
   let page: number = 0;
-  let filtered = (arr: object[]) =>
-    arr
+  let filtered = (assetArr: object[]) =>
+    assetArr
       .slice()
       .filter(
         (value: any, index: number, array: object[]) =>
@@ -23,25 +30,13 @@ export const fetchOrders = async (user: any, assets: object[], setAssets: Functi
       );
 
   const fetchPage = async () => {
-    let newRes: any = await getOrders(localAddress, page);
-    let curr = address || user?.address;
-    if (curr !== localAddress) return;
+    let newRes: any = await getOrders(address, page);
+    if (user?.address !== address) return;
     unique = concat(unique, newRes);
     stop = newRes.length < uniqueLimit || unique.length >= totalLimit;
     page += 1;
-    if (batch) {
-      setAssets(filtered(unique));
-    }
-    if (stop) {
-      if (!batch) {
-        setAssets(filtered(unique));
-      }
-      if (!address) {
-        console.log(unique, localAddress, 'address returned false');
-      }
-    } else {
-      handle = setTimeout(fetchPage, 200);
-    }
+    batch && setAssets(filtered(unique));
+    stop ? !batch && setAssets(filtered(unique)) : (handle = setTimeout(fetchPage, 200));
   };
   fetchPage().catch(err => {
     console.log(err);
