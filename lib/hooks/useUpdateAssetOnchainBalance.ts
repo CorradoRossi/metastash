e618c90cdbd4dfa772233650b588168e9d3e3259
Rossi Meacham <rossi@frugaltricks.com>
2021-08-14 18:20:05 -0400

    done

--- lib/hooks/useUpdateAssetOnchainBalance.ts
+++ lib/hooks/useUpdateAssetOnchainBalance.ts
@@ -11,26 +11,20 @@ export default function useUpdateAssetOnchainBalance() {
 
   const updateAssetOnchainBalance = useCallback(
     async (assetToUpdate, accountAddress, successCallback) => {
-      const balance = await getOnchainAssetBalance(
-        assetToUpdate,
-        accountAddress
-      );
+      const balance = await getOnchainAssetBalance(assetToUpdate, accountAddress);
       if (balance?.amount !== assetToUpdate?.balance?.amount) {
         // Now we need to update the asset
         // First in the state
         successCallback({ ...assetToUpdate, balance });
         // Then in redux
-        const allAssetsUpdated = allAssets.map(asset => {
+        const allAssetsUpdated = allAssets.map((asset: any) => {
           if (asset.address === assetToUpdate.address) {
             asset.balance = balance;
           }
           return asset;
         });
         await dispatch(dataUpdateAssets(allAssetsUpdated));
-        logger.log(
-          `balance updated with onchain data for asset ${assetToUpdate.symbol}`,
-          balance
-        );
+        logger.log(`balance updated with onchain data for asset ${assetToUpdate.symbol}`, balance);
       }
     },
     [allAssets, dispatch]
